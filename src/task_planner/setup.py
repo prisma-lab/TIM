c14640a3dde7555d8deabc1144919f35134b32c5
Yigit Yildirim <yigit.yildirim@bogazici.edu.tr>
2025-06-12 15:37:45 +0300

    planner updates

--- src/task_planner/setup.py
+++ src/task_planner/setup.py
@@ -1,30 +1,25 @@
 from setuptools import setup
-import os
-from glob import glob
 
 package_name = 'task_planner'
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='user@example.com',
-    description='ROS2 task planner package that subscribes to planning requests and uses PlanSys2',
-    license='Apache-2.0',
+    maintainer='Yigit Yildirim',
+    maintainer_email='yigyil@gmail.com',
+    description='A simple task planner node that subscribes to PlanningRequest and publishes plans.',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'task_planner_node = task_planner.task_planner_node:main',
+            'planner_node = task_planner.planner_node:main',
         ],
     },
 )
